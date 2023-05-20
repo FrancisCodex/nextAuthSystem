718571ce0ba062365cf81f94be4908832b7cc1ce
FrancisCodex <altesinggaming@gmail.com>
2023-05-20 21:28:28 +0800

    added navigationbar in index

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
+      <Navbar/>
     <div>
-      <h1>Hello World! Welcome to my Website broskie!!</h1>
+      <h1>Hello World welcome to my website</h1>
     </div>
     </main>
   )
